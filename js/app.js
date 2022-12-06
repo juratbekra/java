e7f2f3b581d1c0b807624fade6df741bdeac3dde
juratbek <juratbekraxmatov515@gmail.com>
2022-12-06 18:03:35 +0500

    chang

---
+++ js/app.js
@@ -0,0 +1,44 @@
+// let h1 = document.createElement('h1')
+// h1.textContent = 'o'
+// h1.className = 'title'
+
+// h1.style.color = 'red'
+// console.dir(h1)
+
+// window.addEventListener('mousemove', function(event){
+//     const x = event.clientX
+//     const y = event.clientY
+
+//     h1.style.position = 'absolute'
+//     // h1.style.transition = '1s'
+
+//     h1.style.top = y + 'px'
+//     h1.style.left = x + 'px'
+
+
+// })
+
+// document.body.append(h1)
+
+// const createElement = (event) => {
+// const div = document.createElement('div')
+// div.style.width = '50px'
+// div.style.height = '50px'
+// div.style.background = 'black'
+// div.style.borderRadius='50%'
+// div.style.position='absolute'
+// div.style.top = event.clientY + 'px'
+// div.style.left = event.clientX + 'px'
+// div.style.transform = 'trenslate(-50%, -50%)'
+
+// return div
+
+
+
+// }
+// window.addEventListener('click', (event) => {
+//     document.body.append(createElement(event))
+// })
+
+
+
